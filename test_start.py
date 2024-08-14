43874730618f274687468c65cca94614eac6514e
Bryan Beecham <bryan_beecham@hotmail.com>
2024-08-14 11:17:51 -0400

    Robiots, World, Map, Point

--- test_start.py
+++ test_start.py
@@ -5,6 +5,9 @@ class Biot:
     def __init__(self):
         self.location = Point(0, 0)
 
+    def update(self, info):
+        self.location = info["location"]
+
 
 class BiotMap:
     pass
@@ -18,6 +21,27 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Entities:
+    def __init__(self):
+        self.contents = {}
+
+    def place(self, biot, location):
+        self.contents[location] = biot
+
+
+class BiotWorld:
+    def __init__(self):
+        self.biots = Entities()
+
+    def add(self, biot):
+        location = Point(10, 10)
+        self.biots.place(biot, location)
+        return {"location": location}
+
 
 class TestCase:
     def test_something(self):
@@ -30,6 +54,14 @@ class TestCase:
     def test_starting_location(self):
         biot = Biot()
         assert biot.location == Point(0, 0)
+
+    def test_biot_in_world(self):
+        biot = Biot()
+        world = BiotWorld()
+        info = world.add(biot)
+        point = info["location"]
+        biot.update(info)
+        assert biot.location == point
 
     @pytest.mark.skip()
     def test_create_biot_map(self):
