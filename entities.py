eb968ab040a4e9482747c8a8996183d9348430f9
Ron Jeffries <ronjeffries@acm.org>
2024-08-19 11:07:20 -0400

    initial mapping test passes

--- entities.py
+++ entities.py
@@ -8,9 +8,27 @@ class Entities:
     def place(self, biot):
         self.contents[biot.id] = biot
 
-    def entity_at(self, column, row):
-        point = Point(column, row)
+    def entity_at(self, x, y):
+        point = Point(x, y)
         for entity in self.contents.values():
             if entity.location == point:
                 return entity
         return None
+
+    def __eq__(self, other):
+        his_contents = other.get_entities()
+        if not self.check_all_his_things_are_valid(his_contents):
+            return False
+        return len(self.contents) == len(his_contents)
+
+    def check_all_his_things_are_valid(self, his_contents):
+        what_he_has_is_correct = True
+        for map_entry in his_contents:
+            his_x = map_entry.x
+            his_y = map_entry.y
+            his_thing = map_entry.entity
+            if self.entity_at(his_x, his_y).name != his_thing.name:
+                what_he_has_is_correct = False
+        return what_he_has_is_correct
+
+
--- test_mapping.py
+++ test_mapping.py
@@ -1,9 +1,16 @@
 from entities import Entities
 
 
+class FakeMap:
+    def get_entities(self):
+        return []
+
+
 class TestMapping:
     def test_hookup(self):
         assert 4 == 2 + 2
 
     def test_map_compare(self):
         map = Entities()
+        other_map = FakeMap()
+        assert map == other_map
