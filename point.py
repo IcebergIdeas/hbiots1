eefb4378535a7ce333326ccddb285429aa25f998
Ron Jeffries <ronjeffries@acm.org>
2024-08-21 09:09:03 -0400

    delete hash

--- point.py
+++ point.py
@@ -6,9 +6,6 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
-    def __hash__(self):
-        return hash((self.x, self.y))
-
     def __repr__(self):
         return f'Point({self.x}, {self.y})'
 
