03b5803174e49572c0c1697a64a373be00da334b
Bryan Beecham <bryan_beecham@hotmail.com>
2024-09-04 09:08:47 -0400

    Removed test

--- test_direction.py
+++ test_direction.py
@@ -5,9 +5,6 @@ class TestDirection:
     def test_hookup(self):
         assert 2 + 2 == 4
 
-    def test_direction_EAST(self):
-        dir = Direction.EAST
-
     def test_direction_left(self):
         east = Direction.EAST
         assert east.left() == Direction.NORTH
@@ -15,4 +12,3 @@ class TestDirection:
     def test_direction_right(self):
         east = Direction.EAST
         assert east.right() == Direction.SOUTH
-
