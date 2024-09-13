c20b8b068f194a4171530ee0a6150df1314da701
Ron Jeffries <ronjeffries@acm.org>
2024-09-13 09:07:10 -0400

    more testing.

--- test_class_per_state_machine.py
+++ test_class_per_state_machine.py
@@ -2,8 +2,9 @@ from machine import Looking
 
 
 class FakeKnowledge():
-    def __init__(self, *, can_take=False):
+    def __init__(self, *, can_take=False, has_block=False):
         self.can_take = can_take
+        self.has_block = has_block
 
 
 class TestClassPerStateMachine:
@@ -16,3 +17,12 @@ class TestClassPerStateMachine:
         state = Looking()
         knowledge = FakeKnowledge(can_take=False)
         assert state.action(knowledge) == []
+
+    def test_looking_update_with_no_block(self):
+        state = Looking()
+        knowledge = FakeKnowledge(has_block=False)
+        machine = None
+        n1, n2, looking = state.update(machine, knowledge)
+        assert n1 is None
+        assert n2 is None
+        assert isinstance(looking, Looking)
