73baf8e9fc9639e2caf5dd85c412807ffb7309fc
Ron Jeffries <ronjeffries@acm.org>
2024-09-14 08:08:12 -0400

    paring down Machine.

--- machine.py
+++ machine.py
@@ -1,6 +1,3 @@
-from knowledge import Knowledge
-
-
 class Walking:
     def action(self, _knowledge):
         return []
@@ -45,71 +42,18 @@ class Laden:
 
 class Machine:
     def __init__(self, knowledge):
-        assert isinstance(knowledge, Knowledge)
         self._knowledge = knowledge
         self._update = None
         self._action = None
         self._state = Walking()
 
     def state(self, knowledge):
-        assert isinstance(knowledge, Knowledge)
         self._knowledge = knowledge
         knowledge.tired -= 1
-        if self._state:
-            info = self._state.update(self, self._knowledge)
-            self._update, self._action, self._state = info
-        else:
-            info = self._update()
-            self._update, self._action, self._state = info
-        if self._state:
-            return self._state.action(self._knowledge)
-        else:
-            return self._action()
-
-    def walking_states(self):
-        return self.walking_update, self.walking_action, None
-
-    def looking_states(self):
-        return None, None, Looking()
-
-    def laden_states(self):
-        return self.laden_update, self.laden_action, None
+        info = self._state.update(self, self._knowledge)
+        self._update, self._action, self._state = info
+        return self._state.action(self._knowledge)
 
     def set_states(self, states):
         self._update, self._action, self._state = states
 
-    def walking_update(self):
-        if self._knowledge.tired <= 0:
-            if self._knowledge.has_block:
-                return None, None, Laden()
-            else:
-                return None, None, Looking()
-        return self.walking_states()
-
-    def walking_action(self):
-        return []
-
-    def looking_update(self):
-        if self._knowledge.has_block:
-            self._knowledge.tired = 5
-            return self.walking_states()
-        else:
-            return self.looking_states()
-
-    def looking_action(self):
-        if self._knowledge.can_take:
-            return ['take']
-        return []
-
-    def laden_update(self):
-        if not self._knowledge.has_block:
-            self._knowledge.tired = 5
-            return self.walking_states()
-        else:
-            return self.laden_states()
-
-    def laden_action(self):
-        if self._knowledge.tired <= 0:
-            if self._knowledge.can_drop:
-                return ['drop']
-        return []
