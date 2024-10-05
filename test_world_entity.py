b80da617cca637c521f638d8b7dbb02f6a9f8cc9
Ron Jeffries <ronjeffries@acm.org>
2024-10-05 09:32:58 -0400

    WorldEntity has dictionary and `id`.

--- test_world_entity.py
+++ test_world_entity.py
@@ -1,8 +1,30 @@
 
 class WorldEntity:
-    pass
+    def __init__(self):
+        self._dict = dict()
+
+    @property
+    def id(self):
+        return self._dict['id']
+
+    @id.setter
+    def id(self, value):
+        self._dict['id'] = value
 
 
 class TestWorldEntity:
+    def is_valid(self, entity):
+        try:
+            assert entity._dict['id'] == entity.id
+            return True
+        except KeyError:
+            return False
+
     def test_create(self):
         WorldEntity()
+
+    def test_set_and_fetch(self):
+        entity = WorldEntity()
+        entity.id = 102
+        assert entity.id == 102
+        assert self.is_valid(entity)
