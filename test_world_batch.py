2b4f4180e8b0ae9a973cb444426caecf47eb115b
Ron Jeffries <ronjeffries@acm.org>
2024-10-15 08:54:54 -0400

    adding tests for `World.process`.

--- test_world_batch.py
+++ test_world_batch.py
@@ -65,3 +65,33 @@ class TestWorldBatch:
         # invasive just to see how we did
         result = batch_out.results[0]
         assert result['location'] == Location(6, 5)
+
+    def test_more_action(self):
+        world = World(10, 10)
+        block_id = world.add_block(6, 5)
+        bot_id = world.add_bot(5, 5, direction=Direction.EAST)
+        request = EntityRequest(bot_id)
+        request.add_action(EntityAction('take', None))
+        request.add_action(EntityAction('turn', 'SOUTH'))
+        request.add_action(EntityAction('step', None))  # 5, 6
+        request.add_action(EntityAction('step', None))  # 5, 7
+        request.add_action(EntityAction('drop', block_id))  # 5, 8
+        batch_in = WorldInput()
+        batch_in.add_request(request)
+        batch_out = world.process(batch_in)
+        result = batch_out.results[0]
+        assert result['location'] == Location(5, 7)
+        item = world.map.at_xy(5, 8)
+        assert item.id == block_id
+
+    def test_two_bots_return_two_results(self):
+        world = World(10, 10)
+        bot_a = world.add_bot(5, 5, direction=Direction.EAST)
+        bot_b = world.add_bot(7, 7, direction=Direction.WEST)
+        request_a = EntityRequest(bot_a)
+        request_b = EntityRequest(bot_b)
+        batch_in = WorldInput()
+        batch_in.add_request(request_a)
+        batch_in.add_request(request_b)
+        batch_out = world.process(batch_in)
+        assert len(batch_out.results) == 2
