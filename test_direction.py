0a06a65d79c1854106c2935cf9b42e81ab891f4c
Ron Jeffries <ronjeffries@acm.org>
2024-10-21 15:18:43 -0400

    edit to allow push

--- test_direction.py
+++ test_direction.py
@@ -4,9 +4,6 @@ from direction import Direction
 
 
 class TestDirection:
-    def test_hookup(self):
-        assert 2 + 2 == 4
-
     def test_direction_left(self):
         east = Direction.EAST
         assert east.left() == Direction.NORTH
