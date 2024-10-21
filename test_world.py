6a4ab2943af57a5b021eeae98d8f53a32f36bf13
Ron Jeffries <ronjeffries@acm.org>
2024-10-21 15:25:40 -0400

    simplify tests since all use same starting location.

--- test_world.py
+++ test_world.py
@@ -14,22 +14,21 @@ def test_take_a_block():
     assert world.is_empty(Location(6, 5))
 
 def test_bot_facing_north_takes_a_north_block():
-    check_directional_take((5, 5), (5, 4), Direction.NORTH)
+    check_directional_take((5, 4), Direction.NORTH)
 
 def test_bot_facing_east_takes_an_east_block():
-    check_directional_take((5, 5), (6,5), Direction.EAST)
+    check_directional_take((6, 5), Direction.EAST)
 
 def test_bot_facing_south_takes_a_south_block():
-    check_directional_take((5, 5), (5, 6), Direction.SOUTH)
+    check_directional_take((5, 6), Direction.SOUTH)
 
 def test_bot_facing_west_takes_a_west_block():
-    check_directional_take((5, 5), (4, 5), Direction.WEST)
+    check_directional_take((4, 5), Direction.WEST)
 
-def check_directional_take(bot_loc, block_loc, direction):
-    bot_x, bot_y = bot_loc
+def check_directional_take(block_loc, direction):
     block_x, block_y = block_loc
     world = World(10, 10)
-    bot_id = world.add_bot(bot_x, bot_y, direction)
+    bot_id = world.add_bot(5, 5, direction)
     bot = world.entity_from_id(bot_id)
     world.add_block(block_x, block_y)
     world.take_forward(bot)
