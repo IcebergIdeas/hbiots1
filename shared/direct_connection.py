00a1c8aca4ac213e2f2cb989299622e60e6aa446
Ron Jeffries <ronjeffries@acm.org>
2024-11-02 10:42:52 -0400

    improve names

--- shared/direct_connection.py
+++ shared/direct_connection.py
@@ -9,21 +9,21 @@ class DirectConnection:
     def add_bot(self, x, y, direction=Direction.EAST):
         bot_id = self.world.add_bot(x, y, direction)
         result_dict = self.world.fetch(bot_id)
-        bot = Bot(x, y)
-        bot._knowledge.update(result_dict)
-        return bot
+        client_bot = Bot(x, y)
+        client_bot._knowledge.update(result_dict)
+        return client_bot
 
-    def set_direction(self, bot, direction_string):
-        self.world.command('turn', bot.id, direction_string)
-        self.update_client(bot)
+    def set_direction(self, client_bot, direction_string):
+        self.world.command('turn', client_bot.id, direction_string)
+        self.update_client(client_bot)
 
-    def update_client(self, bot):
-        result_dict = self.world.fetch(bot.id)
-        bot._knowledge.update(result_dict)
+    def update_client(self, client_bot):
+        result_dict = self.world.fetch(client_bot.id)
+        client_bot._knowledge.update(result_dict)
 
-    def step(self, bot):
-        self.world.command('step', bot.id)
-        self.update_client(bot)
+    def step(self, client_bot):
+        self.world.command('step', client_bot.id)
+        self.update_client(client_bot)
 
     def take(self, client_bot):
         self.world.command('take', client_bot.id)
