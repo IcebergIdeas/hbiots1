7ff7f07e47038b01c58030f34589afcace34a97d
Ron Jeffries <ronjeffries@acm.org>
2024-11-09 09:14:58 -0500

    remove unneeded test

--- tests/test_world_requests.py
+++ tests/test_world_requests.py
@@ -4,22 +4,6 @@ from shared.location import Location
 
 
 class TestWorldRequests:
-    def test_hookup(self):
-        assert True
-
-    def test_imaginary_request(self):
-        single_request = {
-            "entity": 100,
-            "actions": [
-                {"verb": "take"},
-                {"verb": 'step'}
-            ]
-        }
-        id = single_request['entity']
-        assert id == 100
-        first_action = single_request['actions'][0]
-        assert first_action['verb'] == 'take'
-
     def test_one_step(self):
         world = World(10, 10)
         bot_id = world.add_bot(5, 5)
