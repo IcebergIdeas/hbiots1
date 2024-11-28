2f694b7a289ad127b95d15d360605c8baf67cbf5
Ron Jeffries <ronjeffries@acm.org>
2024-11-28 12:40:19 -0500

    tidying

--- server/world.py
+++ server/world.py
@@ -41,39 +41,47 @@ class World:
         match verb:
             case 'add_bot':
                 self.add_bot_action(**parameters)
+            case 'drop':
+                self.drop_forward_action(entity, **parameters)
             case 'step':
                 self.step(entity)
             case 'take':
                 self.take_forward(entity)
-            case 'drop':
-                self.drop_forward_action(entity, **parameters)
-            case 'NORTH' | 'EAST' | 'SOUTH' | 'WEST' as direction:
-                self.set_direction(entity, direction)
             case 'turn':
-                direction = parameters['direction']
-                self.set_direction(entity, direction)
+                self.turn(entity, parameters['direction'])
+            case 'NORTH' | 'EAST' | 'SOUTH' | 'WEST' as direction:
+                self.turn(entity, direction)
             case _:
                 raise Exception(f'Unknown action {verb}')
-
-    def drop_forward_action(self, entity, holding, **_):
-        self.drop_forward(entity, self.entity_from_id(holding))
 
     def add_bot_action(self, x, y, direction, **_):
         bot_id = self.add_bot(x, y, Direction.from_name(direction))
         self.ids_used.add(bot_id)
 
-    def fetch(self, entity_id):
-        return self.entity_from_id(entity_id).as_dictionary()
+    def drop_forward_action(self, entity, holding, **_):
+        self.drop_forward(entity, self.entity_from_id(holding))
 
-    def set_direction(self, world_bot, direction_name):
-        # no change on unrecognized name
-        if direction_name in ['NORTH', 'EAST', 'SOUTH', 'WEST']:
-            world_bot.direction = Direction.from_name(direction_name)
+    def drop_forward(self, bot, entity):
+        if self.map.place_at(entity, bot.forward_location()):
+            bot.remove(entity)
 
     def step(self, bot):
         self.map.attempt_move(bot.id, bot.forward_location())  # changes world version
         self.set_bot_vision(bot)
         self.set_bot_scent(bot)
+
+    def take_forward(self, bot):
+        is_block = lambda e: e.name == 'B'
+        if block := self.map.take_conditionally_at(bot.forward_location(), is_block):
+            bot.receive(block)
+
+    def turn(self, world_bot, direction_name):
+        # no change on unrecognized name
+        if direction_name in ['NORTH', 'EAST', 'SOUTH', 'WEST']:
+            world_bot.direction = Direction.from_name(direction_name)
+
+    def fetch(self, entity_id):
+        return self.entity_from_id(entity_id).as_dictionary()
 
     def set_bot_vision(self, bot):
         bot.vision = self.map.vision_at(bot.location)
@@ -84,18 +92,5 @@ class World:
             aroma_to_seek = bot.holding.aroma
         bot.scent = self.map.scent_at(bot.location, aroma_to_seek)
 
-    def take_forward(self, bot):
-        is_block = lambda e: e.name == 'B'
-        if block := self.map.take_conditionally_at(bot.forward_location(), is_block):
-            bot.receive(block)
-
-    def drop_forward(self, bot, entity):
-        if self.map.place_at(entity, bot.forward_location()):
-            bot.remove(entity)
-
     def is_empty(self, drop_location):
         return not self.map.at_xy(drop_location.x, drop_location.y)
-
-    def update_client_for_test(self, client_bot):
-        result_dict = self.fetch(client_bot.id)
-        client_bot._knowledge.update(result_dict)
